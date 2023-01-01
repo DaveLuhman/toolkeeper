cee01639a986d0126a6983ad597914dcf42dcbe3
Dave Luhman <daveluhman@gmail.com>
2023-01-01 16:00:33 -0600

    Both headers created, condition on if:user

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
 
 
-router.get('/' , (req , res)=>{
-    // router code here
-})
+router.get('/' , (req , res)=>{ res.render('main.hbs')}) // Render Dashboard
+
+//router.get('/badLogin', (_req, res) => {res.render('badLogin', {layout: 'login'})}) // Render Bad Login Page
+// router.get('/logout', (req, res) => {req.logout(() => {}); res.redirect('/')}) // Logout User
+// router.get('/signup', ensureAuth, (_req, res) => {res.render('signUp', {layout: 'login'})})     // Render Sign Up Page
+
+// // @desc POST Routes
+// router.post('/auth', passport.authenticate('local-signin', {successRedirect: '/dashboard', failureRedirect: '/badLogin'})) // Authenticate User
+// router.post('/signup', passport.authenticate('local-signup', {successRedirect: '/dashboard', failureRedirect: '/signup'})) // Create User
 
 
-router.get('/another-route' , (req , res)=>{
-    // router code here
-})
 
-module.exports  = router
+module.exports = router;
+
