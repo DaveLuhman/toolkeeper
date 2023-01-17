50707c9165216f730f3ba970d6d1c13e229a53c5
Dave Luhman <dave@ado.software>
2023-01-17 15:02:33 -0600

    exported router

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,14 @@
-const Router = require("express").Router();
+const router = require('express').Router()
 const api = require("../controllers/api");
 
 // @endpoint /api/
 // @desc Test endpoint
 // @access Public
-Router.get("/", (req, res) => { res.send("ok");})
+router.get("/", (req, res) => { res.send("ok");})
 
 // @endpoint /api/tool/search
 // @desc Search for a tool by SN, PN, BC, or SA
 // @access API
-Router.get("/tool/search", api.search);
+router.get("/tool/search", api.search);
+
+module.exports = router;
