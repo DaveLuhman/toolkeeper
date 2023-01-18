e7d2ebe73223f9b465c8f19ca33b5d9134197cd0
Dave Luhman <daveluhman@gmail.com>
2023-01-17 19:43:37 -0600

    search is workign

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,20 +3,15 @@ const Tool = require('../models/tool')
 c = {}
 
 
-c.renderDashboard = async (req, res) => {
-    console.log('entering dashboard controller')
+c.renderDashboard = (req, res) => {
+    console.log('entering dashboard index controller')
     let perPage = 10
     let page = req.query.p || 1
 
-    // Chcek if tools have already been passed in from search
-    if (req.body.tools != null) {
-        console.log('tools passed in from search')
-        res.render('dashboard', {
-            tools: req.body.tools,
-    })}
+
     console.log(page)
     Tool.find({}).skip((perPage * page) - perPage).limit(perPage)
-           .exec(function (err, tools) {
+        .exec(function (err, tools) {
             Tool.count().exec(function (err, count) {
                 if (err) return next(err)
                 res.render('dashboard', {
@@ -29,5 +24,15 @@ c.renderDashboard = async (req, res) => {
             })
         })
 }
+c.renderSearch = async (req, res) => {
+    console.log('entering dashboard search controller')
+    const { serialNumber, partNumber, barcode, serviceAssignment } = req.body
+    console.log(`search parameters, if any, are ${JSON.stringify(req.body)}`)
+    const tools = await Tool.find({ $or: [{ serialNumber: serialNumber }, { partNumber: partNumber }, { barcode: barcode }, { serviceAssignment: serviceAssignment }] })
+    console.log(`tools found: ${JSON.stringify(tools)}`)
+    if (!tools) { res.render('dashboard', { message: 'No Tool Found Matching Your Search Parameters'}) }
+    res.render('dashboard', { tools: tools, pagination: { pageCount: 0 } })
+}
+
 
 module.exports = c
