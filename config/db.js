682366a50138e5b02ff2fd4cd500fe4e09ad2410
Dave Luhman <daveluhman@gmail.com>
2023-01-18 13:21:03 -0600

    suppressed mongoose update message

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
+        mongoose.set('strictQuery', false);
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold)
     } catch (err) {
         console.log(err.red);
