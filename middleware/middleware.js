d5d8cc82851f762cc2599109e5ae05c951936490
Dave Luhman <daveluhman@gmail.com>
2023-01-18 17:22:29 -0600

    moved pagination into search functions

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,47 +1,53 @@
-const Tool = require('../models/tool')
+const Tool = require('../models/tool');
 
-const middleware = {}
+const middleware = {};
 
 middleware.checkAuth = function (req, res, next) {
     if (req.isAuthenticated()) {
-        res.locals.user = req.user
-        res.locals.pagination = { pageCount: 0}
-        return next()
+        res.locals.user = req.user;
+        res.locals.pagination = { pageCount: 0};
+        return next();
     }
-    res.redirect('/login')
-}
+    res.redirect('/login');
+};
 
 middleware.getTools = async (req, res, next) => {
-    console.log('entering middleware')
-    var tools = null
-    const { serialNumber, partNumber, barcode, serviceAssignment } = req.body
-    console.log(`search parameters, if any, are ${JSON.stringify(req.body)}`)
+    console.log('entering middleware');
+    let perPage = 10;
+    let page = req.query.p || 1;
+    if (req.body.tools) {console.log('tools already being passed in'); next();}
+    var tools;
+    const { serialNumber, partNumber, barcode, serviceAssignment } = req.body;
+    console.log(`search parameters, if any, are ${JSON.stringify(req.body)}`);
     if (serialNumber === "" && partNumber === "" && barcode === "" && serviceAssignment === "") {
-        console.log('leaving middleware without searching')
-    next()
+        console.log('leaving middleware without searching');
+    next();
     }
     if (serialNumber !== "") {
-        console.log(`Searching for ${serialNumber}...`)
-        res.locals.searchTerms = `Serial Number: ${serialNumber}`
-        res.locals.tools = await Tool.find({ serialNumber: serialNumber })
+        console.log(`Searching for ${serialNumber}...`);
+        res.locals.searchTerms = `Serial Number: ${serialNumber}`;
+        res.locals.tools = await Tool.find({ serialNumber: serialNumber }).skip((perPage * page) - perPage).limit(perPage);
     }
     if (partNumber  !== "") {
-        console.log(`Searching for ${partNumber}...`)
-        res.locals.tools = await Tool.find({ partNumber: partNumber })
+        console.log(`Searching for ${partNumber}...`);
+        res.locals.tools = await Tool.find({ partNumber: partNumber }).skip((perPage * page) - perPage).limit(perPage);
     }
     if (barcode  !== "") {
-        console.log(`Searching for ${barcode}...`)
-        res.locals.tools = await Tool.find({ barcode: barcode })
+        console.log(`Searching for ${barcode}...`);
+        res.locals.tools = await Tool.find({ barcode: barcode }).skip((perPage * page) - perPage).limit(perPage);
     }
     if (serviceAssignment  !== "") {
-        console.log(`Searching for ${serviceAssignment}...`)
-        res.locals.tools = await Tool.find({ serviceAssignment: serviceAssignment })
+        console.log(`Searching for ${serviceAssignment}...`);
+        res.locals.tools = await Tool.find({ serviceAssignment: serviceAssignment }).skip((perPage * page) - perPage).limit(perPage);
     }
-    if (!tools) { req.message = 'No Tool Found Matching Your Search Parameters' }
-    console.log('leaving middleware')
-    res.locals.pagination = { pageCount: 1}
-    next()
-}
+    if (!tools) {
+      req.message = 'No Tool Found Matching Your Search Parameters';
+      console.log('leaving search middleware having found no matches');
+      next();
+    }
+    console.log('leaving middleware');
 
+    next();
+};
 
-module.exports = middleware
+module.exports = middleware;
