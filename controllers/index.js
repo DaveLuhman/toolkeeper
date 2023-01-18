d93c0159f66bbb80b4671aa13c5794c09ca2b1c0
Dave Luhman <daveluhman@gmail.com>
2023-01-18 17:02:16 -0600

    linting

--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,27 @@
-const passport = require("passport")
+const passport = require("passport");
 
-controller = {}
+const controller = {};
 
 controller.getIndex = (req, res) => {
-    res.render('index', { layout: 'public.hbs' })
-}
+    res.render('index', { layout: 'public.hbs' });
+};
 
 controller.getLoginPage = (req, res) => {
-    if(req.isAuthenticated()) { res.redirect('/dashboard') }
+    if(req.isAuthenticated()) { res.redirect('/dashboard'); }
     else {
-    res.render('login', { layout: 'login.hbs'}) }}
+    res.render('login', { layout: 'login.hbs'}); }
+};
 
 controller.postLoginPage = (req, res, next) => passport.authenticate('local', {
     successRedirect: '/dashboard',
     failureRedirect: '/login',
     failureFlash: true
-})(req, res, next)
+})(req, res, next);
 
 controller.logUserOut = (req, res) => {
-    req.logout()
-    req.session.destroy()
-    res.redirect('/')
-}
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+};
 
-module.exports = controller
+module.exports = controller;
