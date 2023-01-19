63a1971b8f774193d334ea2df6b7a784f4296acf
Dave Luhman <daveluhman@gmail.com>
2023-01-18 18:38:23 -0600

    linting and cleanup moved render cb to route deprecated controller file

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,37 +1,11 @@
-const Tool = require('../models/tool')
-const User = require('../models/user')
-const toolController = require('../controllers/tool')
-const dashboardController = require('../controllers/dashboard')
-const { checkAuth, getTools } = require('../middleware/middleware')
+const { getTools } = require('../middleware/middleware')
 const router = require('express').Router()
 
-//@target /dashboard/
-//@method GET
-//@desc render dashboard
-router.get('/', dashboardController.renderDashboard)
 
 //@target /dashboard
 //@method POST
-//@desc search for tools then render dashboard
-router.post('/', getTools, dashboardController.renderSearch)
+//@desc search for tools if needed via middleware then render dashboard
+router.use('/', getTools, (req, res) => {res.render('dashboard');});
 
-//@target /dashboard/newTool
-//@desc Open New Tool Modal
-router.get('/userManagement', async (req, res) => {
-    const users = await User.find({})
-    res.render('userManagement', { users: users })
-})
-
-//@target /dashboard/newTool
-//@desc Open Checkout Modal
-router.get('/checkInOut', (req, res) => {
-    res.render('checkInOut', { user: req.user })
-})
-
-//@target /dashboard/newTool
-//@desc Open CheckIn Modal
-router.get('/checkIn', (req, res) => {
-    // router code here
-})
 
 module.exports = router
