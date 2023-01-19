8b468aa2cbeaf50645c4b14db85365bbae8867e8
Dave Luhman <daveluhman@gmail.com>
2023-01-18 21:22:00 -0600

    only csv import left in controller file

--- controllers/tool.js
+++ controllers/tool.js
@@ -1,40 +1,6 @@
 const Tool = require('../models/tool');
 
 const controller = {}
-controller.parseToolToObject = (data) => {
-    const toolObject = {
-        "_id": data._id,
-        "serialNumber": data.serialNumber,
-        "partNumber": data.partNumber,
-        "barcode": data.barcode,
-        "serviceAssignment": data.serviceAssignment,
-        "manufacturer": data.manufacturer,
-        "status": data.status,
-        "description": data.description,
-        "image": data.image,
-        "updatedBy": data.user._id
-    }
-    return toolObject
-}
-
-// @desc     Get all tools
-// @route    GET /api/tools
-// @context  User
-controller.getAllTools = async () => {
-     return  Tool.find({ archived: false })
-}
-
-// @desc     Get unique tool by _id or serial number
-// @route    GET /api/tool
-// @context  User
-controller.getToolByID = async (req, res) => {
-    console.log(req.params.id)
-    let tool = await Tool.findById(req.params.id)
-    if (!tool) { return res.status(404).send('Tool not found') }
-    return res.render('editTool', { tool: tool })
-}
-
-
 
 controller.importFromCSV = async (req, res) => {
     let serviceAssignment
