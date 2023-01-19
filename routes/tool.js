b191243a7f966c9231ff805685800ef2b96d0d7f
Dave Luhman <daveluhman@gmail.com>
2023-01-18 22:31:51 -0600

    added failover route to getByID route

--- routes/tool.js
+++ routes/tool.js
@@ -1,7 +1,10 @@
 const router = require('express').Router()
 const { getTools, createTool, updateTool } = require('../middleware/tool.js')
 
-router.get('/:id', getTools, (_req, res) => {res.render('editTool');})
+router.get('/:id', getTools, (_req, res) => {
+    if(res.locals.tools.length != []) { return res.render('editTool'); }
+    else console.log('not found, redirecting to dashboard'.red)
+    return res.redirect('../dashboard');})
 router.post('/submit', createTool, (_req, res) => {res.render('dashboard');})
 router.post('/update/:id', updateTool, (_req, res) => {res.render('dashboard');})
 
