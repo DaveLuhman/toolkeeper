48de65e90e51857d9fecf92bcf708763957fd1ab
Dave Luhman <daveluhman@gmail.com>
2023-01-20 19:36:37 -0600

    adjusted userMgr routes to match routing convention

--- routes/user.js
+++ routes/user.js
@@ -11,7 +11,7 @@ router.post('/:id', verifySelf, updateUser, getUsers, (_req, res) => { res.rende
 
 router.get('/userManagement/:id', isManager, getUsers, (_req, res) => { res.render('editUser') }) // verify user is manager and show user to edit
 router.post('/userManagement/:id', isManager, updateUser, (_req, res) => { res.render('userManagement') })
-router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement') })
-router.post('/', isManager, createUser, (_req, res) => { res.render('userManagement') })
+router.get('/userManagement', isManager, getUsers, (_req, res) => { res.render('userManagement') })
+router.post('/userManagement/createUser', isManager, createUser, (_req, res) => { res.render('userManagement') })
 
 export default router
