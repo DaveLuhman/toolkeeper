93f83167842a45975d7738e22bfe55f7dc72d95c
Dave Luhman <daveluhman@gmail.com>
2023-01-20 19:36:50 -0600

    playing with how users get defined by session

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,26 +2,23 @@ import passport from 'passport';
 
 function checkAuth(req, res, next) {
     if (req.isAuthenticated()) {
-        res.locals.user = req.user;
         return next();
     }
-    console.log('authenticated: ' + req.isAuthenticated());
     res.redirect('/login');
 }
 function isManager(req, res, next) {
     if (req.user.role == 'User') {
+        console.log('isManager: ' + req.user.role)
         res.status(401).send('Unauthorized');
         return next();
     }
+    console.log('isManager: ' + req.user.role)
     next();
 }
 async function login(req, res, next) {
-    console.log('auth.login middleware');
     passport.authenticate('local', { failureRedirect: '/login', failureFlash: true })
-        (req, res, function () {
-            console.log('auth.login middleware: passport.authenticate');
-            res.redirect('/dashboard');
-        });
+        (req, res, next);
+        res.locals.user = req.user;
     }
 
 function logout(req, res, next) {
