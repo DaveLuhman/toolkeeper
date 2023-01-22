b8ea3d25c2bce55296492dfe963d630a31d46d89
Dave Luhman <daveluhman@gmail.com>
2023-01-21 23:57:37 -0600

    improved logging accuracy and return statements

--- middleware/user.js
+++ middleware/user.js
@@ -23,19 +23,22 @@ async function createUser(req, res, next) {
         res.locals.error = 'Email and Password are required';
         console.warn('Email and Password are both required'.yellow);
         console.info('[MW] createUser-out-1'.bgWhite.blue)
-        return next();
+        res.redirect('back');
+        return;
     }
     if (await User.findOne({ email: email })) {
         res.locals.error = 'Email is already registered';
         console.warn('Email is already registered'.yellow);
         console.info('[MW] createUser-out-2'.bgWhite.blue)
-        return next();
+        res.redirect('back');
+        return;
     }
     if (password != confirmPassword) {
         res.locals.error = 'Passwords do not match';
         console.warn('Passwords do not match'.yellow);
         console.info('[MW] createUser-out-3'.bgWhite.blue)
-        return next();
+        res.redirect('back');
+        return;
     }
     let hash = bcrypt.hashSync(password, 10);
     let newUser = await User.create({ firstName, lastName, email, password: hash, role });
@@ -52,13 +55,17 @@ async function verifySelf(req, res, next) {
         res.status(401)
         res.locals.error = 'Unauthorized'
         console.warn('Unauthorized'.yellow)
+        console.info('[MW] verifySelf-out-0'.bgWhite.blue)
+        res.redirect('back');
+        return;
     }
     console.info('[MW] verifySelf-out-1'.bgWhite.blue)
     return next();
 }
-async function updateUser(req, _res, next) {
-    console.info('[MW] disableUser-in'.bgBlue.white)
-    await User.findByIdAndUpdate(req.params.id, { $set: req.body });
+async function updateUser(req, res, next) {
+    console.info('[MW] updateUser-in'.bgBlue.white)
+    let user = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+    res.locals.user = user;
     console.info('[MW] disableUser-out'.bgWhite.blue)
     return next();
 }
@@ -70,27 +77,20 @@ async function resetPassword(req, res, next) {
     console.info('[MW] resetPassword-in'.bgBlue.white)
     const { _id, password, confirmPassword } = req.body;
     if (!password || !confirmPassword) {
-        res.status(400);
         res.locals.error = 'New password and confirm password are required';
         console.info('[MW] Password and Confirm Password are both required'.yellow);
         console.info('[MW] resetPassword-out-1'.bgWhite.blue)
-
-        return next();
+        res.status(400).redirect('back');
+        return;
     }
     if (password != confirmPassword) {
-        res.status(400);
-        res.locals.error = 'Passwords do not match';
-        console.info('[MW] Password and Confirm Password do not match'.yellow);
+        res.locals.error = 'New password and confirm password must match';
+        console.info('[MW] Password and Confirm Password must match'.yellow);
         console.info('[MW] resetPassword-out-2'.bgWhite.blue)
-        return next();
+        res.status(400).redirect('back');
+        return;
     }
-    if (7 > password.length || password.length > 20) {
-        res.status(400);
-        res.locals.error = 'Password must be between 8 and 20 characters';
-        console.info('[MW] Password must be between 8 and 20 characters'.yellow);
-        console.info('[MW] resetPassword-out-3'.bgWhite.blue)
-        return next();
-    }
+
     let hash = bcrypt.hashSync(password, 10);
     await User.findByIdAndUpdate(_id, { $set: { password: hash } });
     console.info('[MW] resetPassword-out-4'.bgWhite.blue)
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,27 @@
 import { Router as expressRouter } from 'express';
 import { isManager } from '../middleware/auth.js';
-import { createUser, getUsers, updateUser, verifySelf, resetPassword, disableUser } from '../middleware/user.js';
+import { createUser, getUsers, updateUser, resetPassword, disableUser } from '../middleware/user.js';
 
 const router = expressRouter();
- // show user their own profile
-router.get('/profile', verifySelf, (_req, res) => { res.render('profile', {layout: 'user.hbs'}) })
+// show user their own profile
+router.get('/profile', (_req, res) => { res.render('profile', { layout: 'user.hbs' }) })
 // update user's own profile
-router.post('/profile', verifySelf, updateUser, (_req, res) => { res.render('profile', {layout: 'user.hbs'}) })
+router.post('/profile', updateUser, (_req, res) => { res.render('profile', { layout: 'user.hbs' }) })
 // update user's own password
-router.post('/resetPassword', verifySelf, resetPassword, (_req, res) => { res.render('profile', {layout: 'user.hbs'}) })
+router.post('/resetPassword', resetPassword, (_req, res) => { res.render('profile', { layout: 'user.hbs' }) })
 
 // START MANAGER ROUTES
 // show user management page
-router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 // verify user is manager and show user to edit
-router.get('/:id', isManager, getUsers, (_req, res) => { res.render('editUser', {layout: 'user.hbs'}) })
+router.get('/:id', isManager, getUsers, (_req, res) => { res.render('editUser', { layout: 'user.hbs' }) })
 // verify user is manager and update user
 router.post('/:id', isManager, updateUser, (_req, res) => { res.redirect('./') })
 // create new user
-router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 // verify user is manager and disable user
-router.post('/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 // verify user is manager and reset user password
-router.post('/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 
 export default router
