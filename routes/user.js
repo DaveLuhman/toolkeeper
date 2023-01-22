1d84ec65cbff8c5944e5ca8f35ab4d0bbbbacc5b
Dave Luhman <daveluhman@gmail.com>
2023-01-21 22:17:21 -0600

    consolidated routes for /user/userManagement to /user

--- routes/user.js
+++ routes/user.js
@@ -12,16 +12,16 @@ router.post('/resetPassword', verifySelf, resetPassword, (_req, res) => { res.re
 
 // START MANAGER ROUTES
 // verify user is manager and disable user
-router.post('/userManagement/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
 // verify user is manager and reset user password
-router.post('/userManagement/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
 // verify user is manager and update user
-router.post('/userManagement/:id', isManager, updateUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/:id', isManager, updateUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
 // verify user is manager and show user to edit
-router.get('/userManagement/:id', isManager, getUsers, (_req, res) => { res.render('editUser', {layout: 'user.hbs'}) })
+router.get('/:id', isManager, getUsers, (_req, res) => { res.render('editUser', {layout: 'user.hbs'}) })
 // create new user
-router.post('/userManagement/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
 // show user management page
-router.get('/userManagement', isManager, getUsers, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
 
 export default router
