1f4ab58796ffc52dcb205451d003da9935ef953a
Dave Luhman <daveluhman@gmail.com>
2023-01-22 13:08:55 -0600

    updated user pages and modals

--- routes/user.js
+++ routes/user.js
@@ -11,17 +11,17 @@ router.post('/profile', updateUser, (_req, res) => { res.render('profile', { lay
 router.post('/resetPassword', resetPassword, (_req, res) => { res.render('profile', { layout: 'user.hbs' }) })
 
 // START MANAGER ROUTES
-// show user management page
-router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// verify user is manager and reset user password
+router.post('/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// verify user is manager and disable user
+router.post('/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// create new user
+router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 // verify user is manager and show user to edit
 router.get('/:id', isManager, getUsers, (_req, res) => { res.render('editUser', { layout: 'user.hbs' }) })
 // verify user is manager and update user
 router.post('/:id', isManager, updateUser, (_req, res) => { res.redirect('./') })
-// create new user
-router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
-// verify user is manager and disable user
-router.post('/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
-// verify user is manager and reset user password
-router.post('/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// show user management page
+router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 
 export default router
