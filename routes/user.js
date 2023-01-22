8c1b8aedbafd97c7ddbbc7ccbe0558500a272a2e
Dave Luhman <daveluhman@gmail.com>
2023-01-21 22:53:34 -0600

    trying res.redirect again

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,7 @@ router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement
 // verify user is manager and show user to edit
 router.get('/:id', isManager, getUsers, (_req, res) => { res.render('editUser', {layout: 'user.hbs'}) })
 // verify user is manager and update user
-router.post('/:id', isManager, updateUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
+router.post('/:id', isManager, updateUser, (_req, res) => { res.redirect('/') })
 // create new user
 router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', {layout: 'user.hbs'}) })
 // verify user is manager and disable user
