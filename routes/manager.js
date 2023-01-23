cb380ed4cfe36464ed3ff97117338f3fc6147fb3
Dave Luhman <dave@ado.software>
2023-01-23 12:02:31 -0600

    reordered routes

--- routes/manager.js
+++ routes/manager.js
@@ -3,16 +3,17 @@ import { createUser, getUsers, updateUser, resetPassword, disableUser } from '..
 
 const router = expressRouter();
 
-router.post('/resetPW/:id', isManager, resetPassword, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
-// verify user is manager and disable user
-router.post('/disableUser/:id', isManager, disableUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// show user management dashboard
+router.get('/', getUsers, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// get user by ID and render edit page
+router.get('/:id', getUsers, (_req, res) => { res.render('editUser', { layout: 'user.hbs' }) })
+// update user
+router.post('/:id', updateUser, (_req, res) => { res.redirect('./') })
+// reset user's password
+router.post('/resetPW/:id', resetPassword, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+// disable user
+router.post('/disableUser/:id', disableUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 // create new user
-router.post('/createUser', isManager, createUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
-// verify user is manager and show user to edit
-router.get('/:id', isManager, getUsers, (_req, res) => { res.render('editUser', { layout: 'user.hbs' }) })
-// verify user is manager and update user
-router.post('/:id', isManager, updateUser, (_req, res) => { res.redirect('./') })
-// show user management page
-router.get('/', isManager, getUsers, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
+router.post('/createUser', createUser, (_req, res) => { res.render('userManagement', { layout: 'user.hbs' }) })
 
 export default router
