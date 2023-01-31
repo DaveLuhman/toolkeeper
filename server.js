3b71f3b51e5887fb0a18b5c3c58bc776618341e5
Dave Luhman <daveluhman@gmail.com>
2023-01-30 19:37:33 -0600

    updated 'secure' flag to true for cookies

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use(session({
   secret: process.env.SESSION_KEY,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 },
+  cookie: { secure: true, maxAge: 1000 * 60 * 60 * 24 },
   store: store
 }))
 app.use(flash());
