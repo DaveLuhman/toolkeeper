312e46caedd272d026769174433b2d3be833780e
Dave Luhman <dave@ado.software>
2023-03-28 21:40:35 -0500

    Add Authorizer library and implement sign up functionality

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,37 @@
 import passport from 'passport'
-import User from '../models/User.model.js'
+import { Authorizer } from '@authorizerdev/authorizer-js'
 
+const authRef = new Authorizer({
+  clientID: '45e1bfeb-90ef-42e6-ac62-e0712ef5f56a',
+  redirectURL: 'https://auth.dev.ado.software/app',
+  authorizerURL: 'https://auth.dev.ado.software'
+})
+
+export async function register (req, res, next) {
+  const { firstName, lastName, email, password, confirmPassword } = req.body
+  if (!firstName || !lastName || !email || !password || !confirmPassword) {
+    res.locals.error = 'All fields are required'
+    return res.redirect('back')
+  }
+  if (password !== confirmPassword) {
+    res.locals.error = 'Passwords do not match'
+    return res.redirect('back')
+  }
+
+  try {
+    const authRes = await authRef.signUp({
+      email,
+      password,
+      given_name: firstName,
+      family_name: lastName,
+      role: 'User'
+    })
+    console.log(authRes)
+  } catch (err) {
+    res.locals.error = err.message
+    return res.redirect('back')
+  }
+}
 /**
  * @param req Express Request object
  * @param  res  Express Response object
@@ -59,29 +90,6 @@ async function login (req, res, next) {
     failureRedirect: '/login',
     failureFlash: true
   })(req, res, next)
-}
-
-export function register (req, res, next) {
-  const { firstName, lastName, email, password, confirmPassword } = req.body
-  if (!firstName || !lastName || !email || !password || !confirmPassword) {
-    res.locals.error = 'All fields are required'
-    res.status(400).redirect('back')
-  }
-  if (password !== confirmPassword) {
-    res.locals.error = 'Passwords must match'
-    res.status(400).redirect('back')
-  }
-  const hash = bcrypt.hashSync(password, 10)
-  try {
-    User.create({
-      firstName,
-      lastName,
-      email,
-      password: hash
-    })
-  } catch (err) {
-    res.locals.error = 'An error occurred when creating the user'
-  }
 }
 
 /**
