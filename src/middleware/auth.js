b545851ba533b107f6fbcc815281c454da92da61
Dave Luhman <dave@ado.software>
2023-04-04 00:29:52 -0500

    updated register route added error to register page

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,11 +1,5 @@
 import passport from 'passport'
-import { Authorizer } from '@authorizerdev/authorizer-js'
-
-const authRef = new Authorizer({
-  clientID: '45e1bfeb-90ef-42e6-ac62-e0712ef5f56a',
-  redirectURL: 'https://auth.dev.ado.software/app',
-  authorizerURL: 'https://auth.dev.ado.software'
-})
+import User from '../models/User.js'
 
 export async function register (req, res, next) {
   const { firstName, lastName, email, password, confirmPassword } = req.body
@@ -17,18 +11,17 @@ export async function register (req, res, next) {
     res.locals.error = 'Passwords do not match'
     return res.redirect('back')
   }
-
   try {
-    const authRes = await authRef.signUp({
-      email,
-      password,
-      given_name: firstName,
-      family_name: lastName,
-      role: 'User'
+    const user = await User.create({ firstName, lastName, email, password })
+    req.login(user, function (err) {
+      if (err) {
+        return next(err)
+      }
+      return res.redirect('/dashboard')
     })
-    console.log(authRes)
-  } catch (err) {
-    res.locals.error = err.message
+  } catch (error) {
+    console.error(error)
+    res.locals.error = 'Something went wrong'
     return res.redirect('back')
   }
 }
