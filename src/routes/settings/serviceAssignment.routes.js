37605509934425428b5687942bb50c9f383b4700
Dave Luhman <daveluhman@gmail.com>
2023-04-04 20:04:29 -0500

    Split out manager routes (#11)
    * nested settings routes in folder
    added categories
    
    * added route naming convention
    
    * added categories routes and view
    
    * updated SA route and views
    
    * updated userSettings route and views
    
    * edit manager to settings
    
    * nested view
    renamed route to match naming con.
    
    * Add category middleware and routes for CRUD operations on categories
    
    ---------
    
    Co-authored-by: Dave Luhman <dave@ado.software>

---
+++ src/routes/settings/serviceAssignment.routes.js
@@ -0,0 +1,52 @@
+import { Router } from 'express'
+import {
+  getServiceAssignments,
+  getServiceAssignmentByID,
+  updateServiceAssignment,
+  createServiceAssignment,
+  deleteServiceAssignment
+} from '../../middleware/serviceAssignment.js'
+
+export const serviceAssignmentRouter = Router()
+
+// @desc get all service assignments and render settings page
+// @endpoint GET /settings/serviceAssignments
+serviceAssignmentRouter.get('/', getServiceAssignments, (_req, res) => {
+  res.render('settings/serviceAssignments')
+})
+// @desc get service assignment by ID and render edit page
+// @endpoint GET /settings/serviceAssignments/edit/:id
+serviceAssignmentRouter.get(
+  '/edit/:id', // target
+  getServiceAssignmentByID,
+  (_req, res) => {
+    res.render('settings/editServiceAssignment') // render
+  }
+)
+// @desc update service assignment and redirect to settings page
+// @endpoint POST /settings/serviceAssignments/edit
+serviceAssignmentRouter.post(
+  '/edit', // target
+  updateServiceAssignment,
+  (_req, res) => {
+    res.redirect('/settings/serviceAssignments') // redirect
+  }
+)
+// @desc add new service assignment and redirect to settings page
+// @endpoint POST /settings/serviceAssignments/create
+serviceAssignmentRouter.post(
+  '/create',
+  createServiceAssignment,
+  (_req, res) => {
+    res.redirect('/settings/serviceAssignments')
+  }
+)
+// @desc delete service assignment and redirect to settings page
+// @endpoint GET /settings/serviceAssignments/delete/:id
+serviceAssignmentRouter.get(
+  '/delete/:id/',
+  deleteServiceAssignment,
+  (_req, res) => {
+    res.redirect('/settings/serviceAssignments')
+  }
+)
---
+++ src/routes/settings/userSettings.routes.js
@@ -0,0 +1,42 @@
+import { Router } from 'express'
+import {
+  createUser,
+  getUsers,
+  updateUser,
+  resetPassword,
+  disableUser,
+  getUserByID
+} from '../../middleware/user.js'
+
+export const userSettingsRouter = Router()
+
+// @desc get all users and render settings page
+// @endpoint GET /settings/users
+userSettingsRouter.get('/', getUsers, (_req, res) => {
+  res.render('settings/users')
+})
+// @desc get user by ID and render edit page
+// @endpoint GET /settings/users/:id
+userSettingsRouter.get('/:id', getUserByID, (_req, res) => {
+  res.render('settings/editUser')
+})
+// @desc update user and redirect to settings page
+// @endpoint POST /settings/users/:id
+userSettingsRouter.post('/:id', updateUser, (_req, res) => {
+  res.redirect('./')
+})
+// @desc reset another user's password
+// @endpoint POST /settings/users/resetPW/:id
+userSettingsRouter.post('/resetPW/:id', resetPassword, (_req, res) => {
+  res.render('settings')
+})
+// @desc disable user
+// @endpoint POST /settings/users/disableUser/:id
+userSettingsRouter.post('/disableUser/:id', disableUser, (_req, res) => {
+  res.render('settings')
+})
+// @desc create new user
+// @endpoint POST /settings/users/createUser
+userSettingsRouter.post('/createUser', createUser, (_req, res) => {
+  res.render('settings')
+})
