45aaaf306ba8440e3c1653f05f6e5c94423af26f
Dave Luhman <dave@ado.software>
2023-05-30 14:55:59 -0500

    mongoose model/schema linting

--- src/models/Category.model.js
+++ src/models/Category.model.js
@@ -1,25 +1,28 @@
 import { Schema, model } from 'mongoose'
 
-const CategorySchema = new Schema({
-  _id: {
-    type: Schema.Types.ObjectId,
-    auto: true
+const CategorySchema = new Schema(
+  {
+    _id: {
+      type: Schema.Types.ObjectId,
+      auto: true
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    description: {
+      type: String,
+      required: false,
+      maxLength: 64
+    }
   },
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  description: {
-    type: String,
-    required: false,
-    maxLength: 64
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+    timestamps: true
   }
-}, {
-  toObject: { virtuals: true },
-  toJSON: { virtuals: true },
-  timestamps: true
-})
+)
 
 // eslint-disable-next-line new-cap
 export default model('Category', CategorySchema)
