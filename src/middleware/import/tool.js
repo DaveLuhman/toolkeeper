eb45751250db12eeaeffbecffa1727d2c980fba3
Dave Luhman <dave@ado.software>
2023-07-01 14:07:38 -0500

    refactored tool import to match the other two

--- src/middleware/import/tool.js
+++ src/middleware/import/tool.js
@@ -1,25 +1,13 @@
 import Tool from '../../models/Tool.model.js'
-import { importedFileToArrayByRow } from '../util.js'
+import { csvFileToEntries } from '../util.js'
 import ToolHistory from '../../models/ToolHistory.model.js'
 
 function trimArrayValues (array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = array[i].trim()
-  }
-  return array
+  return array.map(cell => cell.trim())
 }
-async function lookupTool (serialNumber) {
-  return Tool.findOne({ serialNumber })
-}
+
 async function checkForDuplicates (serialNumber) {
-  return await lookupTool(serialNumber)
-    .then((result) => {
-      if (!result) return false // didn't find anything, so lookup is falsey
-      else return true
-    }) // found a duplicate serial number
-    .catch((err) => {
-      return err + 'error occured'
-    })
+  return await Tool.exists({ serialNumber })
 }
 
 async function createImportedTool (row) {
@@ -38,25 +26,18 @@ async function createImportedTool (row) {
     if (await checkForDuplicates(toolDocument.serialNumber)) {
       throw new Error('Duplicate serial number')
     }
-    const newTool = await Tool.create(toolDocument)
+    const tool = await new Tool(toolDocument).save()
     await ToolHistory.create({
-      _id: newTool._id
+      _id: tool._id
     })
-    return newTool.id
+    return tool
   } catch (err) {
     console.log(err)
   }
 }
 
 export function importTools (file) {
-  const importDataParentArray = importedFileToArrayByRow(file)
-  const tools = []
-  importDataParentArray.forEach((row) => {
-    return tools.push(row.split(','))
-  })
-  const newTools = []
-  for (let i = 0; i < tools.length; i++) {
-    newTools.push(createImportedTool(tools[i]))
-  }
-  return newTools.length + ' Tools Submitted for import.'
+  const entries = csvFileToEntries(file)
+  entries.map(entry => createImportedTool(entry))
+  return entries.length + ' Tools Submitted for import.'
 }
