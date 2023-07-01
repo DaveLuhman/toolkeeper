ab161b9c9ec498629b06e8beb7fbc680947037bd
Dave Luhman <dave@ado.software>
2023-07-01 13:26:09 -0500

    refactored category import

--- src/middleware/import/categories.js
+++ src/middleware/import/categories.js
@@ -1,51 +1,25 @@
-import { importedFileToArrayByRow } from '../util.js'
+import { csvFileToEntries } from '../util.js'
 import Category from '../../models/Category.model.js'
-function trimArrayValues (array) {
-  for (let i = 0; i < array.length; i++) {
-    array[i] = array[i].trim()
-  }
-  return array
-}
-async function lookupCategory (prefix) {
-  return Category.findOne({ prefix })
-}
-async function checkForDuplicates (prefix) {
-  return await lookupCategory(prefix)
-    .then((result) => {
-      if (!result) return false // didn't find anything, so lookup is falsey
-      else return true
-    }) // found a duplicate entry
-    .catch((err) => {
-      return err + 'error occured'
-    })
+
+function createCategoryDocument (row) {
+  const data = row.map((cell) => cell.trim())
+  return { prefix: data[0], name: data[1], description: data[2] }
 }
 
-async function createCategory (row) {
-  row = trimArrayValues(row)
-  const doc = {
-    prefix: row[0],
-    name: row[1],
-    description: row[2]
-  }
-  try {
-    if (await checkForDuplicates(doc.prefix)) {
-      throw new Error('Duplicate serial number')
-    }
-    await Category.create(doc)
-  } catch (err) {
-    console.log(err)
-  }
+async function saveCategoryDocument (doc) {
+  return await new Category(doc).save()
+}
+
+function createCategories (categories) {
+  const categoryPromises = categories.map((c) => {
+    const categoryDocument = createCategoryDocument(c)
+    return saveCategoryDocument(categoryDocument)
+  })
+  return Promise.allSettled(categoryPromises)
 }
 
 export function importCategories (file) {
-  const importDataParentArray = importedFileToArrayByRow(file)
-  const categories = []
-  importDataParentArray.forEach((row) => {
-    return categories.push(row.split(','))
-  })
-  const newCategories = []
-  for (let i = 0; i < categories.length; i++) {
-    newCategories.push(createCategory(categories[i]))
-  }
-  return newCategories.length + ' Tools Submitted for import.'
+  const categories = csvFileToEntries(file)
+  const successMsg = `${categories.length} Categories Submitted for import.`
+  return createCategories(categories).then(() => ({ successMsg }))
 }
