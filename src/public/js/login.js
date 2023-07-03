8a2779517e3b6e2a22621b85586946b67b5019c3
Dave Luhman <dave@ado.software>
2023-07-03 10:12:23 -0500

    login page started

---
+++ src/public/js/login.js
@@ -0,0 +1,12 @@
+export function emailValidator () {
+  const emailInput = document.getElementById('loginEmailInput')
+
+  emailInput.addEventListener('focusout', (event) => {
+    const emailText = event.target.innerText
+    /*
+    invalid email input
+        syntax
+        
+    */
+  })
+}
