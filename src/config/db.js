e908f7dfb9600c647e5721826f922d411e2f817b
Dave Luhman <dave@ado.software>
2023-07-03 19:22:34 -0500

    added logging and wrapped the whole init in a promise.

--- src/config/db.js
+++ src/config/db.js
@@ -3,33 +3,33 @@ import User from '../models/User.model.js'
 import Category from '../models/Category.model.js'
 import ServiceAssignment from '../models/ServiceAssignment.model.js'
 
-function isUsersCollectionEmpty () {
-  const user = User.find()
-  return user.length > 0
+async function isUsersCollectionEmpty () {
+  const user = await User.count()
+  return user === 0
 }
 
 async function createDefaultUser () {
   try {
-    return new User({
+    const user = await User.create({
       firstName: 'Admin',
       lastName: 'User',
       password: '$2b$10$cDCSqQ17sAbWloBElfevMO9NmjORalQP/1VJ7WY6BwvB7PsuNM./m',
       role: 'Admin',
       email: 'admin@toolkeeper'
     })
+    return user
   } catch (error) {
     console.log(error)
   }
 }
 
 async function createDefaultCategory () {
-  const category = new Category({
+  return await Category.create({
     _id: '64a1c3d8d71e121dfd39b7ab',
     prefix: 'UC',
     name: 'Uncategorized',
     description: 'For Tools that dont have a home'
   })
-  return await category.save()
 }
 
 async function createDefaultServiceAssignments () {
@@ -43,11 +43,13 @@ async function createDefaultServiceAssignments () {
       notes: 'Default SA for imported tools'
     },
     {
+      _id: '64a34b651288871770df1086',
       name: 'DEPOT',
       description: 'Default stockroom for serialized tools',
       type: 'Stockroom'
     },
     {
+      _id: '64a34b651288871770df1087',
       name: 'PARTS',
       description: 'Default stockroom for consumables/parts',
       type: 'Stockroom'
@@ -65,6 +67,10 @@ function createDefaultDocuments () {
   return Promise.allSettled(defaultPromises)
 }
 
+function initializeDatabase () {
+  console.warning('No Users In Database. Initializing Database.\nDefault User is admin@toolkeeper\nDefault password is "asdfasdf"')
+  createDefaultDocuments()
+}
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
@@ -75,7 +81,7 @@ const connectDB = async () => {
     console.error('DB INIT' + err)
     process.exit(1)
   }
-  if (!isUsersCollectionEmpty()) createDefaultDocuments()
+  if (await isUsersCollectionEmpty()) initializeDatabase()
   mongoose.ObjectId.get((v) => v.toString())
 }
 export default connectDB
