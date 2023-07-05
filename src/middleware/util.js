e772b7bd4eb607e274199f381c8431076bd11e70
Dave Luhman <dave@ado.software>
2023-07-05 15:02:36 -0500

    added utility function to return package version

--- src/middleware/util.js
+++ src/middleware/util.js
@@ -119,3 +119,7 @@ export function csvFileToEntries (file) {
     .split(/\r?\n/)
     .map(row => row.split(','))
 }
+
+export function getAppVersionForFooter () {
+  return process.env.npm_package_version
+}
