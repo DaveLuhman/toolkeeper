bc1db5d486b1a171a71f0908159b204b178446f0
Dave Luhman <dave@ado.software>
2023-07-09 04:57:16 -0500

    recreated file that is in fact in use

---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,6 @@
+import { getAllTools } from '../middleware/tool.js'
+import { Router } from 'express'
+export const dashboardRouter = Router()
+dashboardRouter.get('/', getAllTools, (req, res) => {
+  res.render('dashboard')
+})
