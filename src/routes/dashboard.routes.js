3513c1414daacc1a1a89f6e361cae1b1bb551204
Dave Luhman <dave@ado.software>
2023-07-09 04:48:50 -0500

    removed unused file

--- src/routes/dashboard.routes.js
+++
@@ -1,6 +0,0 @@
-import { getAllTools } from '../middleware/tool.js'
-import { Router } from 'express'
-export const dashboardRouter = Router()
-dashboardRouter.get('/', getAllTools, (_req, res) => {
-  res.render('dashboard')
-})
