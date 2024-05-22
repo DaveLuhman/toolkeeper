27cadfb7a619f63e5836cc10f30ec1f1efcf4b41
Dave Luhman <dave@ado.software>
2024-05-22 12:20:58 -0500

    moved terminators to user controller

---
+++ src/controllers/settings/user.js
@@ -0,0 +1,6 @@
+export const renderSettingsUser = (_req, res) => {
+    res.render('settings/users')
+}
+export const renderSettingsEditUser = (_req, res) => {
+    res.render('settings/editUser')
+  }
