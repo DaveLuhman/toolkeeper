04051a3f566bafa81d8a96fdc4dc2af2aea9e708
Dave Luhman <dave@ado.software>
2024-05-22 12:30:08 -0500

    separated concerns to route/middleware/controller per convention

---
+++ src/controllers/settings/material.js
@@ -0,0 +1,6 @@
+export const renderSettingsMaterials = (_req, res) => {
+    res.render('settings/materials')
+}
+export const renderSettingsEditMaterial = (_req, res) => {
+    res.render('settings/editMaterial') // render
+}
