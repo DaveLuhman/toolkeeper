1d8189f1cd48f8ef029a469d2183f09990689fc9
Dave Luhman <dave@ado.software>
2024-10-03 18:37:23 -0500

    switching from early jest testing to mocha jest is all about react and this isn't a react project, mongoose devs recomend mocha, so we'll try that

--- src/tests/jest/setupFile.js
+++
@@ -1,9 +0,0 @@
-beforeAll(async () => {
-	// put your client connection code here, example with mongoose:
-	await mongoose.connect(process.env.MONGO_URI);
-});
-
-afterAll(async () => {
-	// put your client disconnection code here, example with mongoose:
-	await mongoose.disconnect();
-});
