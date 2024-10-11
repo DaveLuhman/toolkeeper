bdedca9b19c7a991bd2a9b350f8915edd9bbbc45
Dave Luhman <dave@ado.software>
2024-10-11 18:03:43 -0500

    updated rate limiter based on reviewers comments.

--- src/middleware/util.js
+++ src/middleware/util.js
@@ -72,7 +72,7 @@ export const rateLimiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  skipSuccessfulRequests: true, // Skip middleware incrementer for successful requests
+  skipSuccessfulRequests: false, // Skip middleware incrementer for successful requests
 })
 
 export const createAccountLimiter = rateLimit({
