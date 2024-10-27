53235e0f0c3159bef9c5dd1810eff437a4a7c36f
Dave Luhman <dave@ado.software>
2024-10-27 13:30:58 -0500

    repaired lemonsqueezy config setup and imports

--- src/config/lemonSqueezy.js
+++ src/config/lemonSqueezy.js
@@ -1,34 +1,35 @@
 export const secret = "df027e53-f33b-4155-8ef2-a7366f65acfe";
-import { lemonSqueezySetup } from '@lemonsqueezy/lemonsqueezy.js';
-lemonSqueezySetup({apiKey: process.env.LEMONSQUEEZY_API_KEY});
-import { lemonSqueezyClient } from '@lemonsqueezy/lemonsqueezy.js';
+import { lemonSqueezySetup, listProducts } from "@lemonsqueezy/lemonsqueezy.js";
+
+lemonSqueezySetup({ apiKey: process.env.LEMONSQUEEZY_API_KEY });
 
 export const toolkeeperProductObject = async () => {
-    try {
-        const { data: products } = await lemonSqueezyClient.listAllProducts();
-        const toolkeeper = products.find(product => product.attributes.slug === "toolkeeper");
+	try {
+		const { data } = await listProducts();
+		const toolkeeper = data.find(
+			(product) => product.attributes.slug === "toolkeeper",
+		);
 
-        if (!toolkeeper) {
-            throw new Error("Toolkeeper product not found");
-        }
+		if (!toolkeeper) {
+			throw new Error("Toolkeeper product not found");
+		}
 
-        return toolkeeper.attributes;
-    } catch (err) {
-        console.error("Error fetching Toolkeeper product:", err.message);
-        throw err;
-    }
+		return toolkeeper.attributes;
+	} catch (err) {
+		console.error("Error fetching Toolkeeper product:", err.message);
+		throw err;
+	}
 };
 
-
 export const checkoutUrl = async () => {
-    const attributes = await toolkeeperProductObject()
-    return attributes.buy_now_url
-}
+	const attributes = await toolkeeperProductObject();
+	return attributes.buy_now_url;
+};
 
 export const inTestMode = async () => {
-	const attributes = await toolkeeperProductObject()
-    return attributes.test_mode
-}
+	const attributes = await toolkeeperProductObject();
+	return attributes.test_mode;
+};
 
-export default toolkeeperProductObject
+export default toolkeeperProductObject;
 // src\config\lemonSqueezy.js
