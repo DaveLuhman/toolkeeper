103dcc0f0ae7d1c9f666026064baf6aaff965a4e
Dave Luhman <dave@ado.software>
2025-01-18 07:21:07 -0600

    eliminated legacy code debt

--- src/public/js/toolkeeper.js
+++ src/public/js/toolkeeper.js
@@ -1,12 +1,3 @@
-function btnToSpinner() {
-	const submitBtn = document.querySelector("#testButton");
-
-	submitBtn.addEventListener("click", () => {
-		submitBtn.outerHTML =
-			'<span class="loading loading-infinity loading-lg"></span>';
-	});
-}
-
 
 function openInNewTab() {
 	const x = globalThis.open();
